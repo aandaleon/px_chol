f4ba9e98db714f9d1b59765a9b64413e5daaed8b
aandaleon <angie.andaleon@gmail.com>
2017-08-16 11:42:32 -0500

    Update 06_runMetaXcan.r

--- 06_runMetaXcan.r
+++ 06_runMetaXcan.r
@@ -5,7 +5,7 @@ database_tissues <- database_tissues$V1
 
 for (i in pheno_name){
   for (j in database_tissues){
-    MetaXcan <- 'python /home/angela/MetaXcan-master/software/MetaXcan.py --model_db_path /home/wheelerlab1/Data/PrediXcan_db/GTEx-V6p-HapMap-2016-09-08/' %&% j %&% '_0.5.db --gwas_folder /home/angela/px_cebu_chol/GEMMA/MichGEMMA/output/' %&% i %&% '/ --snp_column rs --effect_allele_column allele0 --non_effect_allele_column allele1 --chromosome_column chr --position_column ps --freq_column af --beta_column beta --se_column se --pvalue_column p_wald --covariance /home/wheelerlab1/Data/PrediXcan_db/GTEx-V6p-HapMap-2016-09-08/' %&% j %&% '.txt.gz --output_file /home/angela/MetaXcan-master/GEMMA_Cebu/Mich/' %&% j %&%'_0.5.db/' %&% i %&% '.csv'
+    MetaXcan <- 'python /home/angela/MetaXcan-master/software/MetaXcan.py --model_db_path /home/wheelerlab1/Data/PrediXcan_db/GTEx-V6p-HapMap-2016-09-08/' %&% j %&% '_0.5.db --gwas_folder /home/angela/px_cebu_chol/GEMMA/MichGEMMA/output/' %&% i %&% '/ --snp_column rs --effect_allele_column allele1 --non_effect_allele_column allele0 --chromosome_column chr --position_column ps --freq_column af --beta_column beta --se_column se --pvalue_column p_wald --covariance /home/wheelerlab1/Data/PrediXcan_db/GTEx-V6p-HapMap-2016-09-08/' %&% j %&% '.txt.gz --output_file /home/angela/MetaXcan-master/GEMMA_Cebu/Mich/' %&% j %&%'_0.5.db/' %&% i %&% '.csv'
     system(MetaXcan)
   }
 }
