58a3293a6f9fb135cca55c957b4179a374408a54
aandaleon <angie.andaleon@gmail.com>
2017-08-16 09:28:55 -0500

    Update and rename 04_runGEMMA.r to 04_runGEMMA.sh

---
+++ 04_runGEMMA.sh
@@ -0,0 +1,9 @@
+gemma -g /home/angela/px_yri_chol/GEMMA/fullGeno.txt.gz -p /home/angela/px_yri_chol/GEMMA/phenoYRIwoIID2.txt -a /home/angela/px_yri_chol/GEMMA/annoFull.txt -lmm 4 -n 9 -k /home/angela/px_yri_chol/GEMMA/output/PLINKgeno.cXX.txt -o YRI_CHOL_rank
+gemma -g /home/angela/px_yri_chol/GEMMA/fullGeno.txt.gz -p /home/angela/px_yri_chol/GEMMA/phenoYRIwoIID2.txt -a /home/angela/px_yri_chol/GEMMA/annoFull.txt -lmm 4 -n 10 -k /home/angela/px_yri_chol/GEMMA/output/PLINKgeno.cXX.txt -o YRI_HDL_rank
+gemma -g /home/angela/px_yri_chol/GEMMA/fullGeno.txt.gz -p /home/angela/px_yri_chol/GEMMA/phenoYRIwoIID2.txt -a /home/angela/px_yri_chol/GEMMA/annoFull.txt -lmm 4 -n 11 -k /home/angela/px_yri_chol/GEMMA/output/PLINKgeno.cXX.txt -o YRI_TRIG_rank
+gemma -g /home/angela/px_yri_chol/GEMMA/fullGeno.txt.gz -p /home/angela/px_yri_chol/GEMMA/phenoYRIwoIID2.txt -a /home/angela/px_yri_chol/GEMMA/annoFull.txt -lmm 4 -n 12 -k /home/angela/px_yri_chol/GEMMA/output/PLINKgeno.cXX.txt -o YRI_LDL_rank
+
+gemma -g /home/angela/px_cebu_chol/GEMMA/GEMMAinput.txt.gz -p /home/angela/px_cebu_chol/GEMMAphenowoIID.txt -a /home/angela/px_cebu_chol/GEMMA/annoFull.txt -lmm 4 -n 5 -k /home/angela/px_cebu_chol/GEMMA/output/relatedness.cXX.txt -o Cebu_CHOL_rank
+gemma -g /home/angela/px_cebu_chol/GEMMA/GEMMAinput.txt.gz -p /home/angela/px_cebu_chol/GEMMAphenowoIID.txt -a /home/angela/px_cebu_chol/GEMMA/annoFull.txt -lmm 4 -n 6 -k /home/angela/px_cebu_chol/GEMMA/output/relatedness.cXX.txt -o Cebu_HDL_rank
+gemma -g /home/angela/px_cebu_chol/GEMMA/GEMMAinput.txt.gz -p /home/angela/px_cebu_chol/GEMMAphenowoIID.txt -a /home/angela/px_cebu_chol/GEMMA/annoFull.txt -lmm 4 -n 7 -k /home/angela/px_cebu_chol/GEMMA/output/relatedness.cXX.txt -o Cebu_LDL_rank
+gemma -g /home/angela/px_cebu_chol/GEMMA/GEMMAinput.txt.gz -p /home/angela/px_cebu_chol/GEMMAphenowoIID.txt -a /home/angela/px_cebu_chol/GEMMA/annoFull.txt -lmm 4 -n 8 -k /home/angela/px_cebu_chol/GEMMA/output/relatedness.cXX.txt -o Cebu_TRIG_rank
